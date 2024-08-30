378b9f59efb9a139c878e6b6a198aa4823215aea
Kshitij Arora <79055679+kshitijarora04@users.noreply.github.com>
2024-08-30 16:38:49 +0530

    Weather API changed

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 const options = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': 'e38ddd1e9cmsh512203960005a45p1d75eajsn0947c3ceaa6a',
-        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+        'X-RapidAPI-Key': '201ed0206amsh3c1a20e91184765p177709jsn5929f5ff22c2',
+        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
     }
 };
 const getWeather = (city) => {
-    const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city;
+    const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=' + city;
     cityName.innerHTML = city;
     (async () => {
         try {
             const response = await fetch(url, options);
             const result = await response.json();
             console.log(result);
-            feels_like.innerHTML = result.feels_like
-            humidity.innerHTML = result.humidity
-            max_temp.innerHTML = result.max_temp
-            min_temp.innerHTML = result.min_temp
-            sunrise.innerHTML = result.sunrise
-            sunset.innerHTML = result.sunset
-            temp.innerHTML = result.temp
-            wind_degrees.innerHTML = result.wind_degrees
-            wind_speed.innerHTML = result.wind_speed
+            feels_like.innerHTML = result.current.feelslike_c + `&deg;C`
+            humidity.innerHTML = result.current.humidity
+            temp_f.innerHTML = result.current.temp_f + `&deg;F`
+            dew_point.innerHTML=result.current.dewpoint_c+`&deg;C`
+            precip.innerHTML = result.current.precip_mm+` mm`
+            cloud_cov.innerHTML=result.current.cloud+ `%`
+            temp.innerHTML = result.current.temp_c
+            wind_degrees.innerHTML = result.current.wind_degree
+            wind_speed.innerHTML = result.current.wind_kph
 
         } catch (error) {
             console.error(error);
@@ -35,7 +35,7 @@ submit.addEventListener("click", (e) => {
 })
 
 function list1(city) {
-    const url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`;
+    const url = `https://weatherapi-com.p.rapidapi.com/current.json?q=` + city;
 
     (async () => {
         try {
@@ -43,16 +43,15 @@ function list1(city) {
             const result = await response.json();
 
             // Update table cells with data
-            cloud_pct1.innerHTML = result.cloud_pct
-            feels_1.innerHTML = result.feels_like;
-            humidity_1.innerHTML = result.humidity;
-            max_temp1.innerHTML = result.max_temp;
-            min_temp1.innerHTML = result.min_temp;
-            sunrise1.innerHTML = result.sunrise;
-            sunset1.innerHTML = result.sunset;
-            temp_1.innerHTML = result.temp;
-            wind_deg1.innerHTML = result.wind_degrees;
-            wind_speed1.innerHTML = result.wind_speed;
+            cloud_pct1.innerHTML = result.current.temp_c + `&deg;C`
+            temp_1.innerHTML = result.current.temp_f + `&deg;F`
+            feels_1.innerHTML = result.current.feelslike_c + `&deg;C`
+            humidity_1.innerHTML = result.current.humidity +`%`
+            min_temp1.innerHTML = result.current.wind_degree+`&deg`
+            max_temp1.innerHTML = result.current.dewpoint_c+`&deg;C`
+            wind_speed1.innerHTML = result.current.wind_kph+` km/hr`;
+            wind_deg1.innerHTML = result.current.precip_mm+` mm`;
+            sunrise1.innerHTML = result.current.cloud+` %`
         } catch (error) {
             console.error(error);
         }
@@ -61,7 +60,7 @@ function list1(city) {
 list1('Kolkata');
 
 function list2(city) {
-    const url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`;
+    const url = `https://weatherapi-com.p.rapidapi.com/current.json?q=` + city;
 
     (async () => {
         try {
@@ -69,16 +68,15 @@ function list2(city) {
             const result = await response.json();
 
             // Update table cells with data
-            cloud_pct2.innerHTML = result.cloud_pct;
-            feels_2.innerHTML = result.feels_like;
-            humidity_2.innerHTML = result.humidity;
-            max_temp2.innerHTML = result.max_temp;
-            min_temp2.innerHTML = result.min_temp;
-            sunrise2.innerHTML = result.sunrise;
-            sunset2.innerHTML = result.sunset;
-            temp_2.innerHTML = result.temp;
-            wind_deg2.innerHTML = result.wind_degrees;
-            wind_speed2.innerHTML = result.wind_speed;
+            cloud_pct2.innerHTML = result.current.temp_c + `&deg;C`;
+            temp_2.innerHTML = result.current.temp_f + `&deg;F`;
+            feels_2.innerHTML = result.current.feelslike_c + `&deg;C`;
+            humidity_2.innerHTML = result.current.humidity +`%`;
+            min_temp2.innerHTML = result.current.wind_degree+`&deg`;
+            max_temp2.innerHTML = result.current.dewpoint_c+`&deg;C`;
+            wind_speed2.innerHTML = result.current.wind_kph+` km/hr`;
+            wind_deg2.innerHTML = result.current.precip_mm+` mm`;
+            sunrise2.innerHTML = result.current.cloud+` %`;
         } catch (error) {
             console.error(error);
         }
@@ -87,7 +85,7 @@ function list2(city) {
 list2('Mumbai');
 
 function list3(city) {
-    const url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`;
+    const url = `https://weatherapi-com.p.rapidapi.com/current.json?q=` + city;
 
     (async () => {
         try {
@@ -95,16 +93,15 @@ function list3(city) {
             const result = await response.json();
 
             // Update table cells with data
-            cloud_pct3.innerHTML = result.cloud_pct;
-            feels_3.innerHTML = result.feels_like;
-            humidity_3.innerHTML = result.humidity;
-            max_temp3.innerHTML = result.max_temp;
-            min_temp3.innerHTML = result.min_temp;
-            sunrise_3.innerHTML = result.sunrise;
-            sunset_3.innerHTML = result.sunset;
-            temp_3.innerHTML = result.temp;
-            wind_deg3.innerHTML = result.wind_degrees;
-            wind_speed3.innerHTML = result.wind_speed;
+            cloud_pct3.innerHTML = result.current.temp_c + `&deg;C`;
+            temp_3.innerHTML = result.current.temp_f + `&deg;F`;
+            feels_3.innerHTML = result.current.feelslike_c + `&deg;C`;
+            humidity_3.innerHTML = result.current.humidity +`%`;
+            min_temp3.innerHTML = result.current.wind_degree+`&deg`;
+            max_temp3.innerHTML = result.current.dewpoint_c+`&deg;C`;
+            wind_speed3.innerHTML = result.current.wind_kph+` km/hr`;
+            wind_deg3.innerHTML = result.current.precip_mm+` mm`;
+            sunrise3.innerHTML = result.current.cloud+` %`;
         } catch (error) {
             console.error(error);
         }
@@ -113,23 +110,22 @@ function list3(city) {
 list3('Bangalore');
 
 function list4(city) {
-    const url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`;
+    const url = `https://weatherapi-com.p.rapidapi.com/current.json?q=` + city;
 
     (async () => {
         try {
             const response = await fetch(url, options);
             const result = await response.json();
             // Update table cells with data
-            cloud_pct4.innerHTML = result.cloud_pct;
-            feels_4.innerHTML = result.feels_like;
-            humidity_4.innerHTML = result.humidity;
-            max_temp4.innerHTML = result.max_temp;
-            min_temp4.innerHTML = result.min_temp;
-            sunrise_4.innerHTML = result.sunrise;
-            sunset_4.innerHTML = result.sunset;
-            temp_4.innerHTML = result.temp;
-            wind_deg4.innerHTML = result.wind_degrees;
-            wind_speed4.innerHTML = result.wind_speed;
+            cloud_pct4.innerHTML = result.current.temp_c + `&deg;C`;
+            temp_4.innerHTML = result.current.temp_f + `&deg;F`;
+            feels_4.innerHTML = result.current.feelslike_c + `&deg;C`;
+            humidity_4.innerHTML = result.current.humidity +`%`;
+            min_temp4.innerHTML = result.current.wind_degree+`&deg`;
+            max_temp4.innerHTML = result.current.dewpoint_c+`&deg;C`;
+            wind_speed4.innerHTML = result.current.wind_kph+` km/hr`;
+            wind_deg4.innerHTML = result.current.precip_mm+` mm`;
+            sunrise4.innerHTML = result.current.cloud+` %`;
         } catch (error) {
             console.error(error);
         }
